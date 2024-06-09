d161fcd67c83e538ba771b95d229b9dc1419e800
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-06-09 14:35:08 +0200

    Change "group_membership" for_each key to "group:member" to be unique.

--- group_membership.main.tf
+++ group_membership.main.tf
@@ -1,10 +1,10 @@
-resource "aws_identitystore_group_membership" "display_name" {
+resource "aws_identitystore_group_membership" "group_member" {
   for_each = {
     for group_membership in local.group_memberships :
-    group_membership.group => group_membership.member
+    "${group_membership.group}:${group_membership.member}" => group_membership
   }
 
   identity_store_id = var.identity_store_id
-  group_id          = aws_identitystore_group.display_name[each.key].group_id
-  member_id         = aws_identitystore_user.username[each.value].user_id
+  group_id          = aws_identitystore_group.display_name[each.value.group].group_id
+  member_id         = aws_identitystore_user.username[each.value.member].user_id
 }
